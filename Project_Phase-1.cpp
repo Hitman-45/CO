21b8c140280eb1b87c66f7ececd9f5fd6bd3933b
Siddhant Chatse <siddhant130903@gmail.com>
2024-02-25 00:08:16 +0530

    third commit

--- Project_Phase-1.cpp
+++ Project_Phase-1.cpp
@@ -6,57 +6,72 @@
 using namespace std;
 #include "Processor.cpp"
 
-int main() 
+int main()
 {
-    Processor sim(1);
+    Processor sim(2);
 
     // sim.getCore(0).getRegisters()[16] = 0x10000000;
-    
-    sim.setCoreProgram(0, {
-        ".data",
-        "base: .word 5 9 4 3 6 10 2 7 8 1 11 19 18 16 12 15 13 17 20 14",
-        ".text",
-        "Lw 16 base",        
-        "addi 1 0 20",  
-        "addi 2 0 0",    
-        "loop:",
-        "beq 2 1 Exit",    	
-        "addi 3 0 0", 		
-        "Lw 16 base",               
-        "loop2:",
-        "beq 3 1 end2",  
-        "lw 4 0(16)",           
-        "lw 5 4(16)",           
-        "ble 4 5 no_swap",     
-        "sw 5 0(16)",           
-        "sw 4 4(16)",           
-        "no_swap:",
-        "addi 16 16 4",        
-        "addi 3 3 1",          
-        "j loop2",            
-        "end2:",
-        "addi 2 2 1",          
-        "j loop",                  
-        "Exit:",
-        "addi 0 0 0"
-    });
 
+    ifstream inputFile("./bubble_sort.asm");
+    if (!inputFile.is_open())
+    {
+        cerr << "Error: Could not open the file.\n";
+        return 1;
+    }
+
+    vector<string> instructions;
+    string line;
+    while (getline(inputFile, line))
+    {
+        instructions.push_back(line);
+    }
+
+    sim.setCoreProgram(0, instructions);
+
+    sim.setCoreProgram(1, {".data",
+                           "base: .word 5 9 4 3 6 10 2 7 8 1 11 19 18 16 12 15 13 17 20 14",
+                           ".text",
+                           "Lw 16 base",
+                           "addi 1 0 20",
+                           "addi 2 0 0",
+                           "loop:",
+                           "beq 2 1 Exit",
+                           "addi 3 0 0",
+                           "Lw 16 base",
+                           "loop2:",
+                           "beq 3 1 end2",
+                           "lw 4 0(16)",
+                           "lw 5 4(16)",
+                           "ble 4 5 no_swap",
+                           "sw 5 0(16)",
+                           "sw 4 4(16)",
+                           "no_swap:",
+                           "addi 16 16 4",
+                           "addi 3 3 1",
+                           "j loop2",
+                           "end2:",
+                           "addi 2 2 1",
+                           "j loop",
+                           "Exit:",
+                           "addi 0 0 0"});
 
     sim.run();
 
-    for (int i = 0; i < 1; ++i) {
-        const vector<int>& regs = sim.getCoreRegisters(i);
-        for (int reg : regs) {
+    for (int i = 0; i < 2; ++i)
+    {
+        const vector<int> &regs = sim.getCoreRegisters(i);
+        for (int reg : regs)
+        {
             cout << reg << " ";
         }
         cout << endl;
     }
 
     vector<int> v = sim.getMemory();
-    for(int x : v)
+    for (int x : v)
     {
-        if(x != 0)
-            cout<<x<<" ";
+        if (x != 0)
+            cout << x << " ";
     }
     return 0;
 }
