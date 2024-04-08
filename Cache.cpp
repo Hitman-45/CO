6a679fc6ab865ba1c0227fd756501a811c3850b7
Hitman-45 <cs22b005@iittp.ac.in>
2024-04-08 23:27:04 +0530

    Cache class

---
+++ Cache.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+using namespace std;
+struct CacheBlock 
+{
+  uint64_t tag;
+  bool valid;
+};
+
+class CacheSimulator 
+{
+  private:
+    vector<CacheBlock> cache;
+    unsigned int cacheSize;
+    unsigned int blockSize;
+    unsigned int numBlocks;
+
+    pair<uint64_t, uint64_t> splitAddress(uint64_t address) 
+    {
+      // variable address if for byte
+      int num_bits_offset = log2(blockSize);
+      address = address >> num_bits_offset;
+      uint64_t index = address & ((1 << numBlocks) - 1);
+      uint64_t tag = address >> (int)log2(numBlocks);
+      return make_pair(tag, index);
+    }
+
+  public:
+    CacheSimulator(unsigned int _cacheSize, unsigned int _blockSize): cacheSize(_cacheSize), blockSize(_blockSize) 
+    {    
+        if(cacheSize % blockSize == 0);
+        {
+            numBlocks = cacheSize / blockSize;
+            cache.resize(numBlocks);
+        }
+        for (auto b : cache) 
+        {
+            b.valid = false;
+        }
+    }
+
+    bool access (uint64_t address) {
+      // split the address: |---------tag --------------| --index---| --offset---|
+      auto a = splitAddress(address);
+      uint64_t index = a.second;
+      uint64_t tag = a.first;
+
+      if (cache[index].tag == tag && cache[index].valid == true) {
+        return true;
+      }
+      else {
+        cache[index].tag = tag;
+        cache[index].valid = true;
+        return false;
+      }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
